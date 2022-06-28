9576398c108b3d0a9253b09c211ad279daceb4e0
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-06-28 14:11:35 +0300

    hover and opacity, formSubmitHandler

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,36 @@
 let editButton = document.querySelector('.profile__edit-button');
+
 let popupElement = document.querySelector('.popup');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let popupInputName = document.querySelector('.popup__input-name');
-let popupInputAbout = document.querySelector('.popup__input-about');
+let formElement = popupElement.querySelector('.popup__form');
+let closeButton = formElement.querySelector('.popup__close-button');
+let saveButton = formElement.querySelector('.popup__save-button');
+let nameInput = formElement.querySelector('.popup__input-name');
+let jobInput = formElement.querySelector('.popup__input-about');
 
 function popupOpen () {
   popupElement.classList.add('popup_display_open');
 }
+
 editButton.addEventListener('click', popupOpen);
 
-let closeButton = document.querySelector('.popup__close-button');
+
 function popupClose () {
   popupElement.classList.remove('popup_display_open');
 }
+
 closeButton.addEventListener('click', popupClose);
 
-let saveButton = document.querySelector('.popup__save-button');
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  profileName.textContent = popupInputName.value;
-  profileAbout.textContent = popupInputAbout.value;
+
+  let profileName = document.querySelector('.profile__name');
+  let profileJob = document.querySelector('.profile__about');
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
   popupElement.classList.remove('popup_display_open');
 }
+
 saveButton.addEventListener('click', formSubmitHandler);
