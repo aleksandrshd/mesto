8b9f2f25a9dc83780a4c3ae2e7ef972226d58dae
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-07-06 16:40:46 +0300

    добавление исходного перечня карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,47 @@ const closeButton = formElement.querySelector('.popup__close-button');
 const nameInput = formElement.querySelector('.popup__input_type_name');
 const jobInput = formElement.querySelector('.popup__input_type_job');
 
+const elementsList = document.querySelector('.elements__list');
+const elementTemplate = document.querySelector('.element-template').content;
+
+const addButton = document.querySelector('.profile__add-button');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (element) {
+  const Element = elementTemplate.cloneNode(true);
+  Element.querySelector('.elements__image').src = element.link;
+  Element.querySelector('.elements__image').alt = element.name;
+  Element.querySelector('.elements__title').textContent = element.name;
+  elementsList.append(Element);
+})
+
+
 function popupOpen () {
   popupElement.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
