b8408dc9552990529988ae9fccf6430bafee60cc
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-07-21 16:23:13 +0300

    сделана валидация формы добавления места

---
+++ scripts/validate.js
@@ -0,0 +1,48 @@
+const formTitleLink = {
+  form: '.popup__form[name="input_type_titleLink"]',
+  button: '.popup__save-button'
+};
+
+function enableValidation(config) {
+  const form = document.querySelector(config.form);
+  form.addEventListener("submit", handleFormSubmit);
+  form.addEventListener("input", (event) => handleFormInput(event, config));
+
+}
+
+function handleFormSubmit(event) {
+  const form = event.currentTarget;
+  const isValid = form.checkValidity();
+  if (isValid) {
+    alert('Форма валидна');
+  } else {
+    alert('Форма невалидна');
+  }
+};
+
+function handleFormInput(event, config) {
+  const input = event.target;
+  const form = event.currentTarget;
+  showFieldError(input);
+  setSubmitButtonState(form, config);
+};
+
+function showFieldError(input) {
+  const span = input.nextElementSibling;
+  span.textContent = input.validationMessage;
+};
+
+function setSubmitButtonState(form, config) {
+  const button = form.querySelector(config.button);
+  const isValid = form.checkValidity();
+  if (isValid) {
+    button.removeAttribute('disabled');
+    button.classList.remove('popup__save-button_type_disabled');
+  } else {
+    button.setAttribute('disabled', true);
+    button.classList.add('popup__save-button_type_disabled');
+  }
+};
+
+
+enableValidation(formTitleLink);
